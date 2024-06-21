fc6d075ec9df61873366537ca6356d1c6a2e54e0
Yaekirua <111132084+Yaekirua@users.noreply.github.com>
2024-06-21 22:00:35 +0800

    Update header.jsx

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -13,7 +13,7 @@ export default async function Header() {
 		<Navbar isBordered>
 			<NavbarBrand>
 				<p className="text-2xl font-bold">
-					<Link href="/">Dramalama</Link>
+					<Link href="/">YaePlay</Link>
 				</p>
 			</NavbarBrand>
 			<NavbarContent className="hidden sm:flex gap-4" justify="center">
@@ -51,7 +51,7 @@ export default async function Header() {
 					<Button
 						as={Link}
 						color="success"
-						href="https://github.com/real-zephex/Dramalama"
+						href="https://github.com/Yaekirua"
 						variant="faded"
 						target="_blank"
 					>
